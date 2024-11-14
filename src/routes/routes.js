12b3c95f47a042e17684e21fb62768a6064f7e22
prince chouhan <prince.chouhan@oriserve.com>
2024-11-14 22:07:05 +0530

    Revert "Revert "Revert "resolve cores"""
    This reverts commit 52ae552f630fe102cf9512a8f9d4ccd566a7d1ba.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -51,7 +51,6 @@ router.get('/auth/google', (req, res) => {
 router.get('/auth/google/callback',(req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://app.agino.tech");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header('Access-Control-Allow-Methods', 'GET,POST');
   next();
 }, async (req, res) => {
   const { code } = req.query;
